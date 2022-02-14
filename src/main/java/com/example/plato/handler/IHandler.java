113cb46d8ddfc15b521f0bb2f63e662ee88d61dd
zhaodongpo <zhaodongpo@kuaishou.com>
2022-02-14 15:47:33 +0800

    add IHandler.java

---
+++ src/main/java/com/example/plato/handler/IHandler.java
@@ -0,0 +1,14 @@
+package com.example.plato.handler;
+
+
+/**
+ * @author zhaodongpo
+ * @version 1.0
+ * @date 2022/2/14 3:29 下午
+ * <p>
+ * 想要去掉 PreHandler 和 AfterHandler中方法的参数。GraphRunningInfo。
+ */
+public interface IHandler<R> {
+
+
+}
